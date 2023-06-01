a8732beeafd99429207acf72ce5edf2e5f8e729f
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-01 12:26:30 -0500

    feat: apply typography

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!", style = MaterialTheme.typography.body1)
 }
 
 @Preview(showBackground = true)
