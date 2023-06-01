ff5ab13d209c1972095dcaa04b017642db849269
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-01 16:10:45 -0500

    feat: add search screen

---
+++ app/src/main/java/com/example/newsapp/view/ui/screens/search/Search.kt
@@ -0,0 +1,14 @@
+package com.example.newsapp.view.ui.screens.search
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SearchScreen() {
+    SearchContent()
+}
+
+@Composable
+fun SearchContent() {
+    Text(text = "search")
+}
