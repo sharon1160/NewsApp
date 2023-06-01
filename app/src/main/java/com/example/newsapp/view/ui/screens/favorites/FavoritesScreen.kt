fe012b0b069d82cc0d07d053bd18cdeae24d226d
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-01 16:09:13 -0500

    feat: add favorites screen

---
+++ app/src/main/java/com/example/newsapp/view/ui/screens/favorites/FavoritesScreen.kt
@@ -0,0 +1,9 @@
+package com.example.newsapp.view.ui.screens.favorites
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FavoritesScreen(){
+    Text(text = "favorites")
+}
