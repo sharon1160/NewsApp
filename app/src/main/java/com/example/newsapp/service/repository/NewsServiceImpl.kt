30f7cddda48107c6131aad7188045ac0681bfead
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-06 00:19:07 -0500

    feat: add new service

---
+++ app/src/main/java/com/example/newsapp/service/repository/NewsServiceImpl.kt
@@ -0,0 +1,31 @@
+package com.example.newsapp.service.repository
+
+import com.example.newsapp.service.model.Constants
+import com.example.newsapp.service.model.New
+import com.example.newsapp.service.model.NewResponse
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+
+class NewsServiceImpl(private val client: HttpClient): NewsService {
+
+    override suspend fun searchNew(query: String): List<New> {
+        val response = client.get<String> {
+            url {
+                protocol = URLProtocol.HTTPS
+                host = Constants.HOST
+                encodedPath = "/search"
+                parameters.append("api-key", Constants.API_KEY)
+                parameters.append("q", query)
+            }
+        }
+        val apiResponse = Json.decodeFromString<NewResponse>(response)
+        return apiResponse.response.results
+    }
+
+    override fun closeClient() {
+        client.close()
+    }
+}
