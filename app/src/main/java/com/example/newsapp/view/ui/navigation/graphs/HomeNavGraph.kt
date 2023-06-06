f0c3789ee77b80cc120d00dcaef7fef161e713f2
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-06 14:57:57 -0500

    feat: add navigation to detail

--- app/src/main/java/com/example/newsapp/view/ui/navigation/graphs/HomeNavGraph.kt
+++ app/src/main/java/com/example/newsapp/view/ui/navigation/graphs/HomeNavGraph.kt
@@ -1,10 +1,13 @@
 package com.example.newsapp.view.ui.navigation.graphs
 
+import android.net.Uri
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import androidx.room.Room
 import com.example.newsapp.service.data.database.FavoriteDatabase
 import com.example.newsapp.service.repository.FavoritesRepository
@@ -18,7 +21,8 @@ import com.example.newsapp.viewmodel.SearchViewModel
 @Composable
 fun HomeNavGraph(navController: NavHostController) {
     val localContext = LocalContext.current
-    val db = Room.databaseBuilder(localContext, FavoriteDatabase::class.java, "favorites_db").build()
+    val db =
+        Room.databaseBuilder(localContext, FavoriteDatabase::class.java, "favorites_db").build()
     val dao = db.dao
     val repository = FavoritesRepository(dao)
     val favoritesViewModel = FavoritesViewModel(repository)
@@ -29,13 +33,23 @@ fun HomeNavGraph(navController: NavHostController) {
     ) {
         composable(route = BottomBarItem.Search.route) {
             val searchViewModel = SearchViewModel()
-            SearchScreen(searchViewModel, favoritesViewModel)
+            SearchScreen(searchViewModel, favoritesViewModel, navController)
         }
         composable(route = BottomBarItem.Favorites.route) {
-            FavoritesScreen(favoritesViewModel)
+            FavoritesScreen(favoritesViewModel, navController)
         }
-        composable(route = "detail") {
-            DetailScreen()
+        composable(route = "detail/{webTitle}/{thumbnail}/{bodyText}",
+            arguments = listOf(
+                navArgument("webTitle") { type = NavType.StringType },
+                navArgument("thumbnail") { type = NavType.StringType },
+                navArgument("bodyText") { type = NavType.StringType }
+            )
+        ) { backStackEntry ->
+            val webTitle = backStackEntry.arguments?.getString("webTitle").toString()
+            val thumbnail =
+                Uri.parse(Uri.decode(backStackEntry.arguments?.getString("thumbnail"))).toString()
+            val bodyText = backStackEntry.arguments?.getString("bodyText").toString()
+            DetailScreen(webTitle, thumbnail, bodyText)
         }
     }
 }
