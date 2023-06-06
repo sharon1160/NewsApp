6a96282035bd97726cc41a637fd7db2001478a4b
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-06 09:54:39 -0500

    feat: add favorites view model

--- app/src/main/java/com/example/newsapp/view/ui/navigation/graphs/HomeNavGraph.kt
+++ app/src/main/java/com/example/newsapp/view/ui/navigation/graphs/HomeNavGraph.kt
@@ -1,17 +1,27 @@
 package com.example.newsapp.view.ui.navigation.graphs
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.room.Room
+import com.example.newsapp.service.data.database.FavoriteDatabase
+import com.example.newsapp.service.repository.FavoritesRepository
 import com.example.newsapp.view.ui.navigation.BottomBarItem
 import com.example.newsapp.view.ui.screens.detail.DetailScreen
 import com.example.newsapp.view.ui.screens.favorites.FavoritesScreen
 import com.example.newsapp.view.ui.screens.search.SearchScreen
+import com.example.newsapp.viewmodel.FavoritesViewModel
 import com.example.newsapp.viewmodel.SearchViewModel
 
 @Composable
 fun HomeNavGraph(navController: NavHostController) {
+    val localContext = LocalContext.current
+    val db = Room.databaseBuilder(localContext, FavoriteDatabase::class.java, "favorites_db").build()
+    val dao = db.dao
+    val repository = FavoritesRepository(dao)
+    val favoritesViewModel = FavoritesViewModel(repository)
     NavHost(
         navController = navController,
         route = Graph.HOME,
@@ -19,10 +29,10 @@ fun HomeNavGraph(navController: NavHostController) {
     ) {
         composable(route = BottomBarItem.Search.route) {
             val searchViewModel = SearchViewModel()
-            SearchScreen(searchViewModel)
+            SearchScreen(searchViewModel, favoritesViewModel)
         }
         composable(route = BottomBarItem.Favorites.route) {
-            FavoritesScreen()
+            FavoritesScreen(favoritesViewModel)
         }
         composable(route = "detail") {
             DetailScreen()
