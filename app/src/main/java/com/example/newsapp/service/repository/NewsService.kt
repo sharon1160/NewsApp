2c86f8b9ac43a2210f1ff24e990b70fc7be0c51b
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-06 00:18:56 -0500

    feat: add new service

---
+++ app/src/main/java/com/example/newsapp/service/repository/NewsService.kt
@@ -0,0 +1,27 @@
+package com.example.newsapp.service.repository
+
+import com.example.newsapp.service.model.New
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.client.features.logging.*
+
+interface NewsService {
+    suspend fun searchNew(query: String): List<New>
+
+    fun closeClient()
+
+    companion object {
+        fun create(): NewsService {
+            return  NewsServiceImpl(client = HttpClient(Android) {
+                install(Logging) {
+                    level = LogLevel.ALL
+                }
+                install(JsonFeature) {
+                    serializer = KotlinxSerializer()
+                }
+            })
+        }
+    }
+}
