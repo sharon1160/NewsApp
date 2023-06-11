41ad8b212de50e8881158241745317c2cb481243
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-11 18:33:09 -0500

    feat: declare client in implementation

--- app/src/main/java/com/example/newsapp/service/repository/NewsService.kt
+++ app/src/main/java/com/example/newsapp/service/repository/NewsService.kt
@@ -1,30 +1,9 @@
 package com.example.newsapp.service.repository
 
 import com.example.newsapp.service.model.New
-import io.ktor.client.*
-import io.ktor.client.engine.cio.CIO
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.client.plugins.logging.*
-import kotlinx.serialization.json.Json
-import io.ktor.serialization.kotlinx.json.json
 
 interface NewsService {
     suspend fun searchNew(query: String, page: String = "1", filter: String): List<New>
 
     fun closeClient()
-
-    companion object {
-        fun create(): NewsService {
-            return  NewsServiceImpl(client = HttpClient(CIO) {
-                install(Logging) {
-                    level = LogLevel.ALL
-                }
-                install(ContentNegotiation) {
-                    json(Json {
-                        isLenient = true
-                    })
-                }
-            })
-        }
-    }
 }
--- app/src/main/java/com/example/newsapp/service/repository/NewsServiceImpl.kt
+++ app/src/main/java/com/example/newsapp/service/repository/NewsServiceImpl.kt
@@ -7,10 +7,25 @@ import com.example.newsapp.service.model.New
 import com.example.newsapp.service.model.NewResponse
 import io.ktor.client.*
 import io.ktor.client.call.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.logging.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
 
-class NewsServiceImpl(private val client: HttpClient): NewsService {
+class NewsServiceImpl: NewsService {
+    private val client = HttpClient(CIO) {
+        install(Logging) {
+            level = LogLevel.ALL
+        }
+        install(ContentNegotiation) {
+            json(Json {
+                isLenient = true
+            })
+        }
+    }
 
     @kotlinx.serialization.ExperimentalSerializationApi
     override suspend fun searchNew(query: String, page: String, filter: String): List<New> {
